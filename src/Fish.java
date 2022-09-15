90812f9683c86d6979aaf44963114f1313d908d9
Miguel <mgoul@fct.unl.pt>
2022-09-15 11:04:27 +0100

    Updated the Main class to test dogs and cats.

---
+++ src/Fish.java
@@ -0,0 +1,11 @@
+public class Fish implements Animal{
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String speak() {
+        return null;
+    }
+}
